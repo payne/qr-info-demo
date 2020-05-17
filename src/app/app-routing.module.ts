56d2705b30843b5f34c85c275986e58994911e86
Matt Payne <Payne@MattPayne.org>
2020-05-16 20:30:57 -0500

    router works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customer', component: CustomerComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+}
